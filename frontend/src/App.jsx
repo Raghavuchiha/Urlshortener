276832e4c3451dc85229ea16d464cbb8be0fcab7
RAGHAV MANDLOI <raghavmandloi2004@gmail.com>
2026-07-24 15:13:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-   import AuthPage from "./components/AuthPage";
-   import Dashboard from "./components/Dashboard";
-
-   export default function App() {
-     return (
-       <BrowserRouter>
-         <Routes>
-           <Route path="/" element={<AuthPage />} />
-           <Route path="/dashboard" element={<Dashboard />} />
-           <Route path="/reset-password" element={<ResetPasswordPage />} />
-          <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-
-         </Routes>
-       </BrowserRouter>
-     );
-   }
+import AuthPage from "./components/AuthPage";
+import Dashboard from "./components/Dashboard";
+import ForgotPasswordPage from "./components/ForgetPassword";
+import ResetPasswordPage from "./components/ResetPassword";
+ 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+ 
